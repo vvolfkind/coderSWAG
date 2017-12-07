27d56249745b957585b8d6973d729f2d9491970b
Rodo <vvolfkind@gmail.com>
2017-12-07 18:33:31 -0300

    refactorizado

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -22,51 +22,48 @@ class ProductosController extends Controller
 	public function  mostrarMisc(){
 		return view('misc');
 	}
-	public function hboShop(){
-		return view('hbo');
+
+	public function store(Request $request) {
+
+		$rules = [
+			"name" => "required|unique:products",
+			"cost" => "required|numeric",
+			"category_id" => "required|numeric|between:1,3",
+			"fotoPath" => "required",
+		];
+
+		$messages = [
+			"required" => "El :attribute es requerido!",
+			"unique" => "El :attribute tiene que ser único!",
+			"between" => "El :attribute tiene que estar entre :min y :max."
+		];
+
+		$request->validate($rules, $messages);
+
+
+
+		$extensionImagen = $request->file('fotoPath')->getClientOriginalExtension();
+
+		$fotoPath = $request->file('fotoPath')->storeAs('productos', uniqid() . "." . $extensionImagen, 'public');
+		
+
+		$product = \App\product::create([
+			'name' => $request->input('name'),
+			'cost' => $request->input('cost'),
+			'fotopath' => $fotoPath
+		]);
+
+
+
+		$category = \App\categorie::find($request->input('category_id'));
+
+		$product->properties()->sync($request->input('properties'));
+		$product->category()->associate($category);
+		$product->save();
+
+		return redirect('/productos');
 	}
 
-
-public function store(Request $request) {
-
-	$rules = [
-		"name" => "required|unique:products",
-		"cost" => "required|numeric",
-		"category_id" => "required|numeric|between:1,3",
-		"fotoPath" => "required",
-	];
-
-	$messages = [
-		"required" => "El :attribute es requerido!",
-		"unique" => "El :attribute tiene que ser único!",
-		"between" => "El :attribute tiene que estar entre :min y :max."
-	];
-
-	$request->validate($rules, $messages);
-
-
-
-	$extensionImagen = $request->file('fotoPath')->getClientOriginalExtension();
-
-	$fotoPath = $request->file('fotoPath')->storeAs('productos', uniqid() . "." . $extensionImagen, 'public');
-	
-
-	$product = \App\product::create([
-		'name' => $request->input('name'),
-		'cost' => $request->input('cost'),
-		'fotopath' => $fotoPath
-	]);
-
-	
-
-	$category = \App\categorie::find($request->input('category_id'));
-
-	$product->properties()->sync($request->input('properties'));
-	$product->category()->associate($category);
-	$product->save();
-
-	return redirect('/productos');
-}
 	public function traer($id) {
 		
 		$products = \App\product::where('category_id', "=", $id)->get();
@@ -77,7 +74,6 @@ public function store(Request $request) {
 
 	public function todos(){
 		$products = \App\product::all();
-	
 		return view('buzos',compact('products'));
 	}
 
--- app/Http/Controllers/RutaController.php
+++ app/Http/Controllers/RutaController.php
@@ -6,12 +6,12 @@ use Illuminate\Http\Request;
 
 class RutaController extends Controller
 {
-   Public function mostrar(){
-   return view('index');
-   }
-  
-    public function subir(){
-   	return view('subir');
-   }
+	Public function mostrar() {
+		return view('index');
+	}
+
+	public function subir() {
+		return view('subir');
+	}
 
 }
