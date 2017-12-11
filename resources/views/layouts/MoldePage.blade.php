8c8d7f3b22555cc08c200834babed353ce471fa9
SofiaMinissale <sofia.minissale@gmail.com>
2017-12-11 20:06:21 -0300

    descomentado Menu Bar.

--- resources/views/layouts/MoldePage.blade.php
+++ resources/views/layouts/MoldePage.blade.php
@@ -35,10 +35,10 @@
 			</section>
 			</header>
 			<!-- Navigation -->
-			<!--a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle">
+			<a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle">
 				<i class="fa fa-bars"></i>
 			</a>
-			<!--nav id="sidebar-wrapper">
+			<nav id="sidebar-wrapper">
 				<ul class="sidebar-nav">
 					<a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle">
 						<i class="fa fa-times"></i>
@@ -61,7 +61,7 @@
 						 onclick=$( "#menu-close").click();>PRODUCTOS</a>
 					</li>
 				</ul>
-			</nav-->
+			</nav>
 			
 			<section class="section-container col-md-12">
 				@yield("contenido")
