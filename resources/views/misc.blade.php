312e48c06cb9b4bf05b3923abe09692d92b4f596
SofiaMinissale <sofia.minissale@gmail.com>
2017-12-13 16:48:56 -0300

    miscs - Base de datos

--- resources/views/misc.blade.php
+++ resources/views/misc.blade.php
@@ -1,5 +1,7 @@
+
 @extends("layouts.MoldePage")
-@section("contenido")
+
+@section('contenido')
 	<style>
 		.header {
 			display: none;
@@ -11,23 +13,20 @@
 	<div class="gallerylogo text-center">
 		<img data-tilt src="logos/cosashead.png">
 	</div>
-		<div class="productsgrid">
-	@foreach ($miscs as $misc)
-		<hr>
-			<article class="products">
-				<div class="row">
-					<div>
-						<img src="/storage/{{$misc->fotopath}}">
-						<h3>$150</h3>
-						<button type="button" class="btn btn-info">COMPRAR</button>
-					</div>
-				</div>
-			</article>
-	@endforeach
+
+	<div class="productsgrid row">
+		@foreach ($miscs as $misc)
+		<article class="products text-center col-md-4">
+			<img src="/storage/{{$misc->fotopath}}">
+			<h3 >$120</h3>
+			<button type="button" class="btn btn-info">COMPRAR</button>
+		</article>
+		@endforeach
 	</div>
-		<div class="text text-center"><a href="productos">VOLVER</a></div>
+
 	<div class="spacer"></div>
 	<div class="spacer"></div>
-
-
+	<div class="text text-center"><a href="productos">VOLVER</a></div>
+	<div class="spacer"></div>
+	<div class="spacer"></div>
 @endsection
